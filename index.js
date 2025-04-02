523d107b0918146e36d91acef45700f907b0cdee
rafaeli-unoesc <144069663+rafaeli-unoesc@users.noreply.github.com>
2025-04-01 21:10:29 -0300

    implementada a conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 import express from "express";
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize('biblioteca2025', 'postgres', 'postgrespw', {
+    host: 'localhost',
+    port: 32768,
+    dialect: 'postgres',
+    define: {
+        timestamps: false,
+        freezeTableName: true
+    }
+});
+
+try {
+    await sequelize.authenticate();
+    console.log('Conexão com o banco de dados realizada com sucesso.');
+} catch (error) {
+    console.error('Erro ao conectar com o banco de dados:', error);
+}
 
 const app = express();
 app.use(express.json());
