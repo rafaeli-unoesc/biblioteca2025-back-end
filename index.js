ee4d4f72ec8f5cce71e096b226b115cdc17f652b
rafaeli-unoesc <144069663+rafaeli-unoesc@users.noreply.github.com>
2025-04-01 22:22:32 -0300

    implementado método de inserção de editora

--- index.js
+++ index.js
@@ -64,4 +64,13 @@ app.get('/editora/:id', async (req, res) => {
     res.json(respostaBanco);
 });
 
+app.post('/editora', async (req, res) => {
+    //    const nomeeditora = req.body.nomeeditora;
+    //    const cnpj = req.body.cnpj;
+    //    const endereco = req.body.endereco;
+
+    const respostaBanco = await Editora.create(req.body);
+    res.json(respostaBanco);
+});
+
 app.listen(3000, () => { console.log(`Servidor rodando.`) });
